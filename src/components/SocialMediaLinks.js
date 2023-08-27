49363abdc021950dc2c272f3179752f2e06ec87c
BitBeholder <e.sario24@gmail.com>
2023-08-28 01:31:57 +0300

    Fixed build issues

--- src/components/SocialMediaLinks.js
+++ src/components/SocialMediaLinks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faLinkedin, faTelegramPlane } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faLinkedin, faTelegramPlane } from '@fortawesome/free-brands-svg-icons';
 
 const SocialMediaLinks = () => {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.jsx', './src/**/*.js', './public/**/*.html'],
   theme: {
     extend: {
       // Here you can extend tailwind's default configurations, if needed
