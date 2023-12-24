af873305976a17fc58b9b1ac9120b1e5fd605b2a
ni-eminen <matiasnie2@gmail.com>
2023-12-24 17:56:28 +0200

    updated board members

--- src/components/BoardMembers.js
+++ src/components/BoardMembers.js
@@ -1,28 +1,77 @@
 const BoardMembers = () => {
   const members = [
-    { img: '/profiilikuva.png', name: 'Ernesti Sario', role: 'Chairman', email: 'chairman@aaltoai.com', telegram: '@InternetRookie' },
-    { img: '/sampolenkola.png', name: 'Sampo Lenkola', role: 'Vice-chairman & Marketing', email: 'vice-chairman@aaltoai.com', telegram: '@sampolenkola' },
-    { img: 'https://media3.giphy.com/media/7LgKUsZiSjcRO/giphy.gif?cid=ecf05e47rgwc68g4xy1rzvhs1l49mo0r68dw3ojam6t08uc0&ep=v1_gifs_search&rid=giphy.gif&ct=g', name: 'Miikkael Tervonen', role: 'Financier', email: 'finances@aaltoai.com', telegram: '@juhomiikkael' },
-    { img: '/matiasnieminen.png', name: 'Matias Nieminen', role: 'Event Manager', email: 'events@aaltoai.com', telegram: '@matoskni' },
-    { img: '/samulihartikainen.png', name: 'Samuli Hartikainen', role: 'Partnerships manager', email: 'partnerships@aaltoai.com', telegram: '@samuliha' },
-    { img: '/ojaswi.png', name: 'Ojaswi Tyagi', role: 'Community Manager', email: 'community@aaltoai.com', telegram: '@ojaswityagi' },
-    { img: '/behramulukir.png', name: 'Behram Ulukir', role: 'Research & Education Manager', email: 'research-education@aaltoai.com', telegram: '@behramulukir' },
-    { img: '/harshrathee.png', name: 'Harsh rathee', role: 'Relations Manager', email: 'relations@aaltoai.com', telegram: '@HarshRatheee' },
+    {
+      img: "/matias.png",
+      name: "Matias Nieminen",
+      role: "Chairman",
+      email: "chairman@aaltoai.com",
+      telegram: "@matoskni",
+    },
+    {
+      img: "/behramulukir.png",
+      name: "Behram Ulukir",
+      role: "Research & Education Manager",
+      email: "research-education@aaltoai.com",
+      telegram: "@behramulukir",
+    },
+    {
+      img: "/egor.png",
+      name: "Egor Eremin",
+      role: "Financier / Partnerships",
+      email: "finances@aaltoai.com",
+      telegram: "@Gyydis",
+    },
+    {
+      img: "/bruce.png",
+      name: "Bruce Nguyen",
+      role: "Event Manager",
+      email: "events@aaltoai.com",
+      telegram: "@quan_possible",
+    },
+    {
+      img: "/aga.png",
+      name: "Olaus Lintinen",
+      role: "Event Manager",
+      email: "events@aaltoai.com",
+      telegram: "@agaiactaest",
+    },
   ];
 
   return (
     <div className="py-48 flex flex-wrap justify-between items-center">
       {members.map((member, index) => (
-        <div key={index} className="w-full md:w-1/4 p-4 my-12 flex flex-col items-center text-center"> {/* Modified here */}
-          <img src={member.img} alt={member.name} className="w-48 h-48 rounded-full mb-8" />
-          <h2 className="text-xl font-bold mb-2" style={{ fontFamily: 'CAMechano' }}>{member.name}</h2>
-          <p className="mb-1" style={{ fontFamily: 'CAMechano' }}>{member.role}</p>
-          <a href={`mailto:${member.email}`} className="text-gray-400 hover:text-gray-200 mb-1" style={{ fontFamily: 'CAMechano' }}>{member.email}</a>
-          <p style={{ fontFamily: 'CAMechano' }}>{member.telegram}</p>
+        <div
+          key={index}
+          className="w-full md:w-1/4 p-4 my-12 flex flex-col items-center text-center"
+        >
+          {" "}
+          {/* Modified here */}
+          <img
+            src={member.img}
+            alt={member.name}
+            className="w-48 h-48 rounded-full mb-8"
+          />
+          <h2
+            className="text-xl font-bold mb-2"
+            style={{ fontFamily: "CAMechano" }}
+          >
+            {member.name}
+          </h2>
+          <p className="mb-1" style={{ fontFamily: "CAMechano" }}>
+            {member.role}
+          </p>
+          <a
+            href={`mailto:${member.email}`}
+            className="text-gray-400 hover:text-gray-200 mb-1"
+            style={{ fontFamily: "CAMechano" }}
+          >
+            {member.email}
+          </a>
+          <p style={{ fontFamily: "CAMechano" }}>{member.telegram}</p>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default BoardMembers;
