2331a355d57538d25ddc50a18077c316f1747c24
ni-eminen <matiasnie2@gmail.com>
2024-01-18 18:27:10 +0200

    icon sizes and order

--- src/components/SocialMediaLinks.js
+++ src/components/SocialMediaLinks.js
@@ -5,14 +5,14 @@ import { faInstagram, faLinkedin, faTelegramPlane } from '@fortawesome/free-bran
 const SocialMediaLinks = () => {
   return (
     <div className="flex flex-col lg:flex-row justify-center items-center space-y-4 lg:space-y-0 lg:space-x-6">
-      <a href="https://www.instagram.com/aaltoaisociety/" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faInstagram} className="text-white text-2xl hover:text-gray-400" />
-      </a>
       <a href="https://www.linkedin.com/company/aaltoai" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faLinkedin} className="text-white text-2xl hover:text-gray-400" />
+        <FontAwesomeIcon icon={faLinkedin} className="h-14 text-white text-2xl hover:text-gray-400" />
       </a>
-      <a href="https://t.me/+GATsXRAQzO5jZmE0" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faTelegramPlane} className="text-white text-2xl hover:text-gray-400" />
+      <a href="https://t.me/+IX9h1s5hpysxMjE8" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faTelegramPlane} className="h-20 mx-6 text-white text-2xl hover:text-gray-400" />
+      </a>
+      <a href="https://www.instagram.com/aaltoaisociety/" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faInstagram} className="h-14 text-white text-2xl hover:text-gray-400" />
       </a>
     </div>
   );
