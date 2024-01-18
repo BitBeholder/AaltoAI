57400b78f4c7a74f78f1cc1c18e32ac7ca787459
ni-eminen <matiasnie2@gmail.com>
2024-01-18 18:18:40 +0200

    bigger icons!

--- src/components/SocialMediaLinks.js
+++ src/components/SocialMediaLinks.js
@@ -6,13 +6,13 @@ const SocialMediaLinks = () => {
   return (
     <div className="flex flex-col lg:flex-row justify-center items-center space-y-4 lg:space-y-0 lg:space-x-6">
       <a href="https://www.instagram.com/aaltoaisociety/" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faInstagram} className="text-white text-2xl hover:text-gray-400" />
+        <FontAwesomeIcon icon={faInstagram} className="h-20 text-white text-2xl hover:text-gray-400" />
       </a>
       <a href="https://www.linkedin.com/company/aaltoai" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faLinkedin} className="text-white text-2xl hover:text-gray-400" />
+        <FontAwesomeIcon icon={faLinkedin} className="h-20 mx-6 text-white text-2xl hover:text-gray-400" />
       </a>
       <a href="https://t.me/+PYA-PJ6tOcE5NjZk" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faTelegramPlane} className="text-white text-2xl hover:text-gray-400" />
+        <FontAwesomeIcon icon={faTelegramPlane} className="h-20 text-white text-2xl hover:text-gray-400" />
       </a>
     </div>
   );
