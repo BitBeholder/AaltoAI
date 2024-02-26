9a214426a591aaf4bfde88e10027b2e19864517a
ni-eminen <matiasnie2@gmail.com>
2024-02-26 10:51:22 +0200

    rules

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React from "react";
+import Pdf from "./aaltoai-rules.pdf";
 
 const Footer = () => {
-    return (
-        <footer className="bg-black text-white">
-            <div className="container mx-auto">
-                {/* GIF Section */}
-                <div className="mb-8 flex justify-center">
-                    <img src="https://media0.giphy.com/media/xEQdDzzHsVhK3Mhcqa/giphy.gif?cid=ecf05e470he8te6osar0nz20ldoecrwzsa34q32i2nptcmxt&ep=v1_gifs_related&rid=giphy.gif&ct=g" alt="Description of GIF" className="w-1/4" />
-                </div>
+  return (
+    <footer className="bg-black text-white">
+      <div className="container mx-auto">
+        {/* GIF Section */}
+        <div className="mb-8 flex justify-center">
+          <img
+            src="https://media0.giphy.com/media/xEQdDzzHsVhK3Mhcqa/giphy.gif?cid=ecf05e470he8te6osar0nz20ldoecrwzsa34q32i2nptcmxt&ep=v1_gifs_related&rid=giphy.gif&ct=g"
+            alt="Description of GIF"
+            className="w-1/4"
+          />
+        </div>
+        <p className="text-center text-sm py-48 w-full">
+          <a href={Pdf} target="_blank">
+            Download Rules (Finnish)
+          </a>
+        </p>
 
-                {/* Other footer contents can go here */}
-                <p className="text-center text-sm py-48">&copy; 2023 AaltoAI. All rights reserved.</p>
-            </div>
-        </footer>
-    );
-}
+        {/* Other footer contents can go here */}
+        <p className="text-center text-sm py-48">
+          &copy; 2023 AaltoAI. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
